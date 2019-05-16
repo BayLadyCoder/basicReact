5af705f3aec8cbaced0f0c1770378188639c1725
BayLadyCoder <rachada_32@hotmail.com>
2019-05-16 19:27:58 -0400

    conditional in JSX

--- index.js
+++ index.js
@@ -41,18 +41,56 @@ function JSXDemo() {
 		<div>
 			<h1>JSX Demo</h1>
 			<h2>from Function Component</h2>
-			<h3>
-				Embeded JSX
-				<br />
-				Calculating with JS: {4 * 5}, {80 - 9}
-			</h3>
-			<h3>
-				JS functions
-				<br />
-				My Current Mood is: {getMood()}
-			</h3>
+
+			<div>
+				<h3>Embeded JSX</h3>
+				<h4>
+					Calculating with JS: {4 * 5}, {80 - 9}
+				</h4>
+			</div>
+			<hr />
+			<div>
+				<h3>JS functions</h3>
+				<h4>My Current Mood is: {getMood()}</h4>
+			</div>
 			<img src="http://www.petmd.com/sites/default/files/what-does-it-mean-when-cat-wags-tail.jpg" />
 		</div>
+	);
+}
+
+// -------------------------------------------------------
+
+function getNum() {
+	return Math.floor(Math.random() * 10 + 1);
+}
+
+function NumPicker() {
+	const num = getNum();
+	let msg;
+	if (num === 7) {
+		msg = (
+			<img src="https://media1.giphy.com/media/xThuWpoG470Q0stGmI/source.gif" />
+		);
+	} else {
+		msg = <p>Don't give up!</p>;
+	}
+	return (
+		<div>
+			<h1>Conditional in JSX</h1>
+			<h3>Your Number is {num}</h3>
+			<p>{msg}</p>
+		</div>
+		/* 
+           {num === 7 ? (
+				<img src="https://media1.giphy.com/media/xThuWpoG470Q0stGmI/source.gif" />
+			) : null}
+
+            OR (if the first condition is True, img will show, else img not show)
+
+            {num === 7 && 
+				<img src="https://media1.giphy.com/media/xThuWpoG470Q0stGmI/source.gif" />
+			}
+         */
 	);
 }
 
