3651909e914d9a433a6d515b54db24b9bb7cf52d
BayLadyCoder <rachada_32@hotmail.com>
2019-05-16 23:28:57 -0400

    Props are immutable

--- Props/Hello.js
+++ Props/Hello.js
@@ -1,7 +1,19 @@
 class Hi extends React.Component {
 	render() {
+		
 		console.log(this.props);
 		const props = this.props;
 		return <h1>Hi {props.to}, from {props.from}!</h1>;
 	}
 }
+
+/*
+	 Props are immutable
+	 for example,
+		this.props.to = "Ringo"
+		this.props.age = 9
+	these code will not work, it will give you an error
+	 
+	props is read-only
+
+ */
